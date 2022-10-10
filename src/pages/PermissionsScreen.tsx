6615f28d16a6d7f1becbae921454a56793a69073
joseFastyDev <jose.rangel@fastydev.com>
2022-10-10 18:05:18 -0500

    10oct2022

---
+++ src/pages/PermissionsScreen.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { BlackButton } from '../components/BlackButton';
+import { PermissionsContext } from '../context/PermissionsContext'
+
+export const PermissionsScreen = () => {
+
+    const { permissions, askLocationPermission } = useContext( PermissionsContext );
+
+    return (
+        <View style={ styles.container }>
+            <Text>PermissionsScreen</Text>
+
+            <BlackButton
+                title="Permiso"
+                onPress={ askLocationPermission }
+            />
+
+            <Text>
+                { JSON.stringify(permissions, null, 5)}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
